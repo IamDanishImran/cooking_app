939257caef9f736e60fe4608a64f04e5ef804114
Danish Imran <imran24022004@gmail.com>
2025-06-09 01:55:14 +0800

    Update auth button

--- components/auth-button.jsx
+++ components/auth-button.jsx
@@ -6,26 +6,45 @@ import { ThemeSwitcher } from "./theme-switcher";
 
 export async function AuthButton() {
   const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  if (!user) {
+    return (
+      <div className="flex gap-2">
+        <Button asChild size="lg" variant={"outline"}>
+          <Link href="/auth/login">Sign in</Link>
+        </Button>
+        <Button asChild size="lg" variant={"default"}>
+          <Link href="/auth/sign-up">Sign up</Link>
+        </Button>
+        <ThemeSwitcher className="border border-black" />
+      </div>
+    );
+  }
 
-  return user ? (
+  const { data: profile, error } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("user_id", user.id)
+    .single();
+
+  if (error) {
+    console.error("Error fetching profile:", error.message);
+    // fallback to just email
+    // return (
+    //   <div className="flex items-center gap-4">
+    //     Hey, {user.email}!
+    //     <LogoutButton />
+    //     <ThemeSwitcher className="border border-black" />
+    //   </div>
+    // );
+  }
+
+  return (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {profile?.username ?? user.email}!
       <LogoutButton />
-      <ThemeSwitcher className="border border-black"/>
-    </div>
-  ) : (
-    <div className="flex gap-2">
-      <Button asChild size="lg" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
-      </Button>
-      <Button asChild size="lg" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
-      </Button>
-      <ThemeSwitcher className="border border-black"/>
+      <ThemeSwitcher className="border border-black" />
     </div>
   );
 }
